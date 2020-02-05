275749d570d30817310926c5a75708a6d7eb4280
Peckidz <60695673+Peckidz@users.noreply.github.com>
2020-02-05 12:32:32 +0100

    Add files via upload

---
+++ Homework.js
@@ -0,0 +1,80 @@
+        //Function number 1
+function m2k(num){ 
+        var result = num / 0.62137;
+        console.log(result)
+}   
+    console.log("Function for converting miles to kilometers")
+    m2k(2)
+
+
+        //Function number 2
+function k2m(num){
+        var result = num * 0.62137;
+        console.log(result)
+}
+    console.log("Function for converting kilometers to miles")
+    k2m(4)
+
+            //Function number 3
+function d2h(num){ 
+    var result = num * 24;
+    console.log(result)
+}   
+console.log("Function for converting days to hours")
+d2h(2)
+
+        //Function number 4
+        function e2d(num){ 
+            var result = num * 1.10;
+            console.log(result)
+    }   
+        console.log("Function for converting euros to dollars")
+        e2d(10)
+
+                //Function number 5
+function y2w(num){ 
+    var result = num * 52;
+    console.log(result)
+}   
+console.log("Function for converting years to weeks")
+y2w(4)
+
+        //Function number 6
+        function P2L(num){ 
+            var result = num * 0.473176;
+            console.log(result)
+    }   
+        console.log("Function for converting Pints to Liters")
+        P2L(20)
+
+                //Function number 7
+function Pa2Kpa(num){ 
+    var result = num * 0.001;
+    console.log(result)
+}   
+console.log("Function for converting Pascal to Kilopascal")
+Pa2Kpa(2000)
+
+        //Function number 8
+        function gb2mb(num){ 
+            var result = num * 1024;
+            console.log(result)
+    }   
+        console.log("Function for converting Gigabytes to Megabytes")
+        gb2mb(6)
+
+                //Function number 9
+function dte(num){ 
+    var result = num / 61.5;
+    console.log(result)
+}   
+console.log("Function for converting Denar to Euro")
+dte(6150)
+
+        //Function number 10
+        function eu2d(num){ 
+            var result = num * 61.5;
+            console.log(result)
+    }   
+        console.log("Function for converting Euro to Denar")
+        eu2d(100)
